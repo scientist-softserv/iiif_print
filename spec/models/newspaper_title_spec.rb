5fd88ed2022677ca57925ba4f2d6150e5d79a77d
Sean Upton <sdupton@gmail.com>
2018-04-18 15:09:26 -0600

    NewspaperTitle and shared metadata mixin tests

---
+++ spec/models/newspaper_title_spec.rb
@@ -0,0 +1,47 @@
+# Generated via
+#  `rails generate hyrax:work NewspaperTitle`
+require 'spec_helper'
+require 'model_shared'
+
+RSpec.describe NewspaperTitle do
+
+  before(:all) do
+    @fixture = model_fixtures(NewspaperTitle)
+  end
+
+  # shared behaviors
+  it_behaves_like("a work and PCDM object")
+  it_behaves_like("a persistent work type")
+
+  describe "Model behaviors and properties" do
+    it "class has expected properties" do
+      properties = NewspaperTitle.properties
+      expect(properties.keys).to include "alternative_title"
+    end
+  end
+
+  describe "Relationship methods" do
+    it "has expected test fixture" do
+      expect(@fixture).to be_an_instance_of(NewspaperTitle)
+    end
+
+    it "can get issues" do
+      issues = @fixture.issues
+      expect(issues).to be_an_instance_of(Array)
+      # our test fixture is non-empty
+      expect(issues.length).to be > 0
+      issues.each do |e|
+        expect(e).to be_an_instance_of(NewspaperIssue)
+      end
+    end
+  end
+
+  it "can get containers" do
+    containers = @fixture.containers
+    expect(containers).to be_an_instance_of(Array)
+    expect(containers.length).to be > 0
+    containers.each do |e|
+      expect(e).to be_an_instance_of(NewspaperContainer)
+    end
+  end
+end
---
+++ spec/models/newspaper_works/newspaper_core_metadata_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe "NewspaperCoreMetadata" do
+
+  before do
+    class NewspaperishWork < ActiveFedora::Base
+      include ::Hyrax::WorkBehavior
+      include NewspaperWorks::NewspaperCoreMetadata
+      include ::Hyrax::BasicMetadata
+    end
+  end
+
+  it "creates work using mixin" do
+    work = NewspaperishWork.new
+    expect(work).to be_an_instance_of(NewspaperishWork)
+  end
+
+  it "has expected properties" do
+    properties = NewspaperishWork.properties
+    expect(properties.keys).to include "genre"
+    expect(properties.keys).to include "held_by"
+    expect(properties.keys).to include "issued"
+    expect(properties.keys).to include "place_of_publication"
+  end
+
+  it "work can set/get properties" do
+    work = NewspaperishWork.new
+    work.genre = ['http://cv.iptc.org/newscodes/genre/Opinion']
+    expect(work.genre).to include 'http://cv.iptc.org/newscodes/genre/Opinion'
+  end
+
+  it "work using mixin saves" do
+    work = NewspaperishWork.new
+    work.place_of_publication = ['http://www.geonames.org/5780993/about.rdf']
+    work.genre = ['http://cv.iptc.org/newscodes/genre/Opinion']
+    expect(work.id).to be_nil
+    work.save!
+    expect(work.id).to_not be_nil
+    expect(NewspaperishWork.all.map { |w| w.id }).to include(work.id)
+  end
+end
