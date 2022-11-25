a0e2672c8c9ecf245344e0ea60bd80da2da73a7e
Rob Kaufman <rob@notch8.com>
2022-11-24 22:51:01 -0800

    fix test generator

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -2,7 +2,7 @@
 require 'rails/generators'
 require 'byebug'
 class TestAppGenerator < Rails::Generators::Base
-  source_root './spec/test_app_templates'
+  source_root File.expand_path('../../../spec/test_app_templates', __dir__)
 
   def install_redis
     gem 'redis', '4.8.0'
@@ -18,11 +18,12 @@ class TestAppGenerator < Rails::Generators::Base
   # TODO not sure why this doesnt work
   # just copy them manually for the moment
   def install_config_files
-    debugger
     copy_file 'blacklight.yml', 'config/blacklight.yml'
-    copy_file 'fcrepo.yml', 'config/fcrepo.yml'
+    copy_file 'fedora.yml', 'config/fedora.yml'
     copy_file 'redis.yml', 'config/redis.yml'
     copy_file 'solr.yml', 'config/solr.yml'
+    copy_file 'solr/conf/schema.xml', 'solr/conf/schema.xml'
+    copy_file 'solr/conf/solrconfig.xml', 'solr/conf/solrconfig.xml'
   end
 
   def install_engine
