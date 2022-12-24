89ca9c7eb37f0f4edbd431c99ce5ea72fbb267fb
Kirk Wang <kirk.wang@scientist.com>
2022-12-24 10:57:17 -0800

    put back some configurations

--- lib/iiif_print/configuration.rb
+++ lib/iiif_print/configuration.rb
@@ -1,9 +1,20 @@
 module IiifPrint
   class Configuration
-    attr_writer :models_to_be_excluded_from_search
-    def models_to_be_excluded_from_search
-      return @models_to_be_excluded_from_search unless @models_to_be_excluded_from_search.nil?
-      @models_to_be_excluded_from_search = []
+    attr_writer :excluded_model_name_solr_field_values
+    # By default, this uses an array of human readable types
+    #   ex: ['Generic Work', 'Image']
+    # @return [Array<String>]
+    def excluded_model_name_solr_field_values
+      return @excluded_model_name_solr_field_values unless @excluded_model_name_solr_field_values.nil?
+      @excluded_model_name_solr_field_values = []
+    end
+
+    attr_writer :excluded_model_name_solr_field_key
+    # A string of a solr field key
+    # @return [String]
+    def excluded_model_name_solr_field_key
+      return "human_readable_type_sim" unless defined?(@excluded_model_name_solr_field_key)
+      @excluded_model_name_solr_field_key
     end
   end
 end
