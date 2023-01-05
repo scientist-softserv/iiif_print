96471bc2091cf76faa954406843602101a70ca57
Shana <shana.lavina.moore@gmail.com>
2023-01-04 16:11:59 -0800

    [i31] - copy over SetParentDefaultThumbnailJob from LV and update generator

---
+++ app/jobs/set_default_parent_thumbnail_job.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class SetDefaultParentThumbnailJob < ApplicationJob
+  queue_as :import
+
+  def perform(parent_work:, importer_run_id:)
+    importer_run = Bulkrax::ImporterRun.find(importer_run_id)
+    parent_work.reload
+    return if parent_work.thumbnail.present?
+
+    curation_concerns = Hyrax.config.curation_concerns
+    return unless curation_concerns.include?(parent_work.class)
+
+    # the representative thumbnail for the parent should be the first page sorted alphanumerically by source_identifier
+    sorted_children = parent_work.child_works.sort_by { |work| work.identifier.first }
+    child_file_set = sorted_children&.first&.file_sets&.first
+    if child_file_set.nil?
+      reschedule(parent_work: parent_work, importer_run_id: importer_run_id)
+      return false # stop current job from continuing to run after rescheduling
+    end
+
+    parent_work.representative = child_file_set
+    parent_work.thumbnail = child_file_set
+    parent_work.save
+    # rubocop:disable Rails/SkipsModelValidations
+    importer_run.increment!(:processed_parent_thumbnails)
+  rescue ::StandardError => e
+    importer_run.increment!(:failed_parent_thumbnails)
+    # rubocop:enable Rails/SkipsModelValidations
+    Bulkrax::Entry.find_by(identifier: parent_work.identifier.first).status_info(e) if parent_work
+  end
+
+  def reschedule(parent_work:, importer_run_id:)
+    SetDefaultParentThumbnailJob.set(wait: 5.minutes)
+                                .perform_later(parent_work: parent_work, importer_run_id: importer_run_id)
+  end
+end
---
+++ lib/generators/iiif_print/templates/create_relationships_job_decorator.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# OVERRIDE: Bulkrax v.3.5
+
+module Bulkrax
+  module CreateRelationshipsJobDecorator
+    attr_accessor :child_records, :parent_record, :parent_entry, :importer_run_id
+
+    def create_relationships
+      if parent_record.is_a?(::Collection)
+        collection_parent_work_child unless child_records[:works].empty?
+        collection_parent_collection_child unless child_records[:collections].empty?
+      else
+        work_parent_work_child unless child_records[:works].empty?
+        child_records[:works].each do |work|
+          # reindex filesets to update solr's is_page_of_ssim
+          work.file_sets.each(&:update_index)
+        end
+        # OVERRIDE: Bulkrax v.3.0
+        # set the first child's thumbnail as the thumbnail for the parent
+        IiifPrint::SetDefaultParentThumbnailJob.set(wait: 10.minutes)
+                                      .perform_later(parent_work: parent_record, importer_run_id: importer_run_id)
+
+        if child_records[:collections].present?
+          raise ::StandardError, 'a Collection may not be assigned as a child of a Work'
+        end
+      end
+    end
+  end
+end
+
+Bulkrax::CreateRelationshipsJob.prepend(Bulkrax::CreateRelationshipsJobDecorator)
