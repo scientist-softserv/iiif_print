ffd74e210f8390252b958ba0d525dc45ff7f81e4
Kirk Wang <kirk.wang@scientist.com>
2023-01-24 15:52:55 -0800

    Remove #total_pages and save for another PR

--- app/presenters/iiif_print/work_show_presenter_decorator.rb
+++ app/presenters/iiif_print/work_show_presenter_decorator.rb
@@ -2,14 +2,6 @@
 
 module IiifPrint
   module WorkShowPresenterDecorator
-    # OVERRIDE: Hyrax 2.9.6
-    def total_pages(members = nil)
-      # if we're hiding the derivatives, we won't have as many pages to show
-      pages = members.presence || total_items
-
-      (pages.to_f / rows_from_params.to_f).ceil
-    end
-
     delegate :file_set_ids, to: :solr_document
 
     # OVERRIDE Hyrax 2.9.6 to remove check for representative_presenter.image? and allow
