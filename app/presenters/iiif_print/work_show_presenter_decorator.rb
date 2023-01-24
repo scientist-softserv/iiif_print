db1a30274169a871708ff5b6216858841cbae195
Kirk Wang <kirk.wang@scientist.com>
2023-01-24 11:12:07 -0800

    Make UV show up if parent has no file set
    This commit will bring over the `WorkShowPresenterDecorator` and
    the affected view partial from Louisville.  This decorator allows for
    the Universal Viewer to show up on a parent work that has a child work
    with a file set, but does not have its own file set.  Previously, the
    UV would not render.
    
    Reference file:
    https://github.com/scientist-softserv/louisville-hyku/blob/main/app/presenters/hyrax/work_show_presenter_decorator.rb

---
+++ app/presenters/iiif_print/work_show_presenter_decorator.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module IiifPrint
+  module WorkShowPresenterDecorator
+    # OVERRIDE: Hyrax 2.9.6
+    def total_pages(members = nil)
+      # if we're hiding the derivatives, we won't have as many pages to show
+      pages = members.presence || total_items
+
+      (pages.to_f / rows_from_params.to_f).ceil
+    end
+
+    delegate :file_set_ids, to: :solr_document
+
+    # OVERRIDE Hyrax 2.9.6 to remove check for representative_presenter.image? and allow
+    # a fallback to check for images on the child works
+    # @return [Boolean] render a IIIF viewer
+    def iiif_viewer?
+      parent_work_has_files || child_work_has_files
+    end
+
+    private
+
+    def parent_work_has_files
+      Hyrax.config.iiif_image_server? &&
+        representative_id.present? &&
+        representative_presenter.present? &&
+        members_include_viewable_image?
+    end
+
+    def child_work_has_files
+      file_set_ids.present?
+    end
+  end
+end
