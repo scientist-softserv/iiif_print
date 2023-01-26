7b2e0d380c586c28e5a506a2476a606efe622fbd
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-01-26 11:16:33 -0500

    Removing PDF Composer job and constituent parts
    This commit removes portions of the Newspaper Works feature that
    stitched together a PDF from provided TIFFs.  An assumption of the IIIF
    Print project is that we are not assembling a PDF from the associated
    files.  That is the domain of the newspaper_works gem.
    
    Reviewers, please not I *think* we don't need this feature.  And we
    should compare this against our proposed feature list (which is
    presently being reviewed by other folks).

--- lib/iiif_print/errors.rb
+++ lib/iiif_print/errors.rb
@@ -6,9 +6,4 @@ module IiifPrint
   # Data transformation or read-error:
   class DataError < IiifPrintError
   end
-
-  # Specific exception for temporary state where one or more PDF page source
-  #   files are not ready, for which a retry at a later time is warranted.
-  class PagesNotReady < DataError
-  end
 end
