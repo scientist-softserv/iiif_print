bcfa9a9a2c7a503ce304dffdc9d19bca28583b73
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-02-09 09:13:57 -0500

    Removing `first_page_bsi` from catalog generator
    Prior to this commit, we were carrying over the `first_page_bsi` field
    from the newspaper_works gem.
    
    Looking at that gem there is the following in index behavior:
    
    ```bash
    app/indexers/concerns/newspaper_works/indexes_relationships.rb
    90:      solr_doc['first_page_bsi'] = true if this_page_index.zero?
    ```
    
    When I run `rg first_page` prior to this commit	the only "hit" is found
    in the generator method that I'm removing.
    
    After this commit, there are no more references to `first_page_bsi`.
    
    Related to:
    
    - https://github.com/scientist-softserv/iiif_print/issues/121

--- lib/generators/iiif_print/catalog_controller_generator.rb
+++ lib/generators/iiif_print/catalog_controller_generator.rb
@@ -18,15 +18,5 @@ module IiifPrint
         "    config.add_index_field 'all_text_tsimv', highlight: true, helper_method: :render_ocr_snippets\n"
       end
     end
-
-    def add_facets_to_catalog_controller
-      marker = 'configure_blacklight do |config|'
-      inject_into_file 'app/controllers/catalog_controller.rb', after: marker do
-        "\n\n    # IiifPrint facet fields\n"\
-        "    # additional IiifPrint fields not displayed in the facet list,\n"\
-        "    # but below definitions give labels to filters for linked metadata\n"\
-        "    config.add_facet_field 'first_page_bsi', label: 'First page', if: false\n"
-      end
-    end
   end
 end
