7a0c72cce0cd238b10acf6f6f19e032981e1b219
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-02-13 10:33:34 -0500

    Removing generated files and commented code
    Prior to this commit, we generated code into the downstream application.
    This code included two injected modules: `SearchBehavior` and
    `AnnotationBehavior`.  In the case of `SearchBehavior` we had switching
    logic based on models that came from the `newspaper_works` gem.
    
    With this commit, I removed the `SearchBehavior` installation.  It is not
    something we need given the current constraints.
    
    And instead of installing a module to extend the
    `BlacklightIiifSearch::Annotation` via the "behavior" pattern, we're
    directly prepending a decorator to the class which included the
    `AnnotationBehavior` (e.g. cut out the middle module).
    
    Last I removed a commented out line; it was additional chatter we didn't need.
    
    Related to:
    
    - https://github.com/scientist-softserv/iiif_print/issues/121

--- lib/generators/iiif_print/templates/annotation_behavior.rb
+++
@@ -1,6 +0,0 @@
-# customizable behavior for IiifSearch
-module BlacklightIiifSearch
-  module AnnotationBehavior
-    include IiifPrint::BlacklightIiifSearch::AnnotationBehavior
-  end
-end
--- lib/generators/iiif_print/templates/search_behavior.rb
+++
@@ -1,6 +0,0 @@
-# customizable behavior for IiifSearch
-module BlacklightIiifSearch
-  module SearchBehavior
-    include IiifPrint::BlacklightIiifSearch::SearchBehavior
-  end
-end
