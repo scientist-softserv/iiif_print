b07dff66569bd243e8bb2f163edbee69210ed866
Kirk Wang <kirk.wang@scientist.com>
2023-03-17 09:31:30 -0700

    Add metadata to the parent
    Previously, the metadata would always sit on the child canvas.  This
    would fail to render any metadata for the parent work.  This commit will
    fix that.

--- app/presenters/iiif_print/iiif_manifest_presenter_behavior.rb
+++ app/presenters/iiif_print/iiif_manifest_presenter_behavior.rb
@@ -3,6 +3,12 @@ module IiifPrint
   module IiifManifestPresenterBehavior
     extend ActiveSupport::Concern
 
+    def manifest_metadata
+      current_ability = try(:ability) || try(:current_ability)
+      base_url = try(:base_url) || try(:request)&.base_url
+      IiifPrint.manifest_metadata_for(work: model, current_ability: current_ability, base_url: base_url)
+    end
+
     def search_service
       Rails.application.routes.url_helpers.solr_document_iiif_search_url(id, host: hostname)
     end
