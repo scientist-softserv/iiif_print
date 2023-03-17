11508b69d19db7f6fa877b3d2a0fe01c3cb3a2da
LaRita Robinson <larita@scientist.com>
2023-03-17 17:38:42 -0400

    Cleanup from homepage search builder

--- lib/iiif_print/homepage_search_builder.rb
+++ lib/iiif_print/homepage_search_builder.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
-require 'hyrax/homepage_search_builder'
 
 # Overrides Hyrax to add show_parents_only to processor chain
 module IiifPrint
   class HomepageSearchBuilder < Hyrax::HomepageSearchBuilder
-    include Hyrax::FilterByType
-    self.default_processor_chain += [:add_access_controls_to_solr_params, :show_parents_only]
-
-    def only_works?
-      true
-    end
+    self.default_processor_chain += [:show_parents_only]
 
     def show_parents_only(solr_parameters)
       query = if blacklight_params["include_child_works"] == 'true'
