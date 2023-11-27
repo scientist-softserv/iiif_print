82d96987d57b961fbe240c68da008d5604d5fc1f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-11-27 09:28:18 -0500

    🧹 Add .internal_test_app
    Prior to this commit, when we'd push this to CI, we'd continue to
    rebuild the `.internal_test_app`; this resulted in 2 hour build times.
    
    By adding it to the repository, we explicitly say when we want to
    rebuild the internal test application (e.g. by deleting it and letting a
    local `bundle exec rake` rebuild it).
    
    See the README.md for more details.

---
+++ .internal_test_app/config/initializers/iiif_print.rb
@@ -0,0 +1,17 @@
+IiifPrint.config do |config|
+  # NOTE: WorkTypes and models are used synonymously here.
+  # Add models to be excluded from search so the user
+  # would not see them in the search results.
+  # by default, use the human readable versions like:
+  # @example
+  #   # config.excluded_model_name_solr_field_values = ['Generic Work', 'Image']
+  #
+  # config.excluded_model_name_solr_field_values = []
+
+  # Add configurable solr field key for searching,
+  # default key is: 'human_readable_type_sim'
+  # if another key is used, make sure to adjust the
+  # config.excluded_model_name_solr_field_values to match
+  # @example
+  #   config.excluded_model_name_solr_field_key = 'some_solr_field_key'
+end
