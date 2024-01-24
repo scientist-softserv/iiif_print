cb5aa7374cf6c8c30edb1966d929da3284f5946c
Shana Moore <shana@scientist.com>
2024-01-24 08:07:45 -0800

    🧹 Add guard clauses to migrations
    Check if table exists before insisting the migration should run.

--- db/migrate/20190107165909_create_iiif_print_ingest_file_relations.rb
+++ db/migrate/20190107165909_create_iiif_print_ingest_file_relations.rb
@@ -1,11 +1,13 @@
 class CreateIiifPrintIngestFileRelations < ActiveRecord::Migration[5.0]
   def change
-    create_table :iiif_print_ingest_file_relations do |t|
-      t.string :file_path
-      t.string :derivative_path
+    unless table_exists?(:iiif_print_ingest_file_relations)
+      create_table :iiif_print_ingest_file_relations do |t|
+        t.string :file_path
+        t.string :derivative_path
 
-      t.timestamps
+        t.timestamps
+      end
+      add_index :iiif_print_ingest_file_relations, :file_path
     end
-    add_index :iiif_print_ingest_file_relations, :file_path
   end
 end
