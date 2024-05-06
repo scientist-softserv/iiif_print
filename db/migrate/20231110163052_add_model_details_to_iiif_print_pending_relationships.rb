9bab4967c28c805337887eed9b8e8a637431b90f
LaRita Robinson <larita@scientist.com>
2024-05-06 13:02:14 -0400

    Fix migration typo

--- db/migrate/20231110163052_add_model_details_to_iiif_print_pending_relationships.rb
+++ db/migrate/20231110163052_add_model_details_to_iiif_print_pending_relationships.rb
@@ -1,7 +1,7 @@
 class AddModelDetailsToIiifPrintPendingRelationships < ActiveRecord::Migration[5.2]
   def change
-    add_column :iiif_print_pending_relationships, :parent_model, :string unless column_exists?(:iif_print_pending_relationships, :parent_model)
-    add_column :iiif_print_pending_relationships, :child_model, :string  unless column_exists?(:iif_print_pending_relationships, :child_model)
-    add_column :iiif_print_pending_relationships, :file_id, :string  unless column_exists?(:iif_print_pending_relationships, :file_id)
+    add_column :iiif_print_pending_relationships, :parent_model, :string unless column_exists?(:iiif_print_pending_relationships, :parent_model)
+    add_column :iiif_print_pending_relationships, :child_model, :string  unless column_exists?(:iiif_print_pending_relationships, :child_model)
+    add_column :iiif_print_pending_relationships, :file_id, :string  unless column_exists?(:iiif_print_pending_relationships, :file_id)
   end
 end
