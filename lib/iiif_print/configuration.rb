7b9767d93fe9f6001b8d7289d35cf1f713443faf
Shana Moore <shana@scientist.com>
2024-06-21 15:32:18 -0700

    Replace #lease_release_date with #lease_expiration_date
    #lease_release_date is not a valid method so threw an error when running jobs to create relationships. Replace method with #lease_expiration_date.
    
    Valid lease methods include:
    
    ```
    parent_work.lease.methods.grep(/lease/)=>
    [:visibility_after_lease=,
     :lease_history=,
     :lease_expiration_date,
     :lease_expiration_date=,
     :lease_history,
     :visibility_after_lease,
     :visibility_during_lease,
     :visibility_during_lease=]
    ```

--- lib/iiif_print/configuration.rb
+++ lib/iiif_print/configuration.rb
@@ -212,7 +212,7 @@ module IiifPrint
             visibility: 'lease',
             visibility_after_lease: lease.visibility_after_lease,
             visibility_during_lease: lease.visibility_during_lease,
-            lease_release_date: lease.lease_release_date
+            lease_release_date: lease.lease_expiration_date
           }
         else
           visibility_params = { visibility: parent_work.visibility.to_s }
